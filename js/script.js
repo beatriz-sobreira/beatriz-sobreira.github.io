c791b5e8ab4874528e89e88091eebcaefaa2f735
beatriz-sobreira <beatrizrsobreira@gmail.com>
2025-07-24 01:56:34 -0300

    feat: implement dark mode toggle and remove unused dark mode files

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
+// download file button
+
 document.getElementById("download-cv").addEventListener("click", function () {
-    const link = document.createElement("a");
-    link.href = "../docs/beatriz_rolim_sobreira_cv_english_version.pdf";
-    link.download = "beatriz_rolim_sobreira_cv_english_version.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const link = document.createElement("a");
+  link.href = "../docs/beatriz_rolim_sobreira_cv_english_version.pdf";
+  link.download = "beatriz_rolim_sobreira_cv_english_version.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 });
+
+
+
+// shrink menu bar after scrolling
 
 window.addEventListener('scroll', () => {
   const header = document.querySelector('.header');
   
-  if(window.scrollY > 50) {  // Se rolou 50px ou mais para baixo
+  if(window.scrollY > 50) {  
     header.classList.add('shrink');
   } else {
     header.classList.remove('shrink');
   }
+});
+
+
+
+// menu mode: light mode and dark mode
+
+let mode = document.getElementById('header-menu-mode');
+let body = document.querySelector('body');
+
+mode.addEventListener('click', () => {
+  mode.classList.toggle('dark');
+  body.classList.toggle('dark');
 });
